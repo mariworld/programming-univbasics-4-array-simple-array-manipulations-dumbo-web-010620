96808271b2db3eb7cdfc617e52973046aaf47ca8
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-29 16:17:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,8 +46,8 @@ def using_flatten(instruments)
 end
 
 instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-def using_delete(instructors)
-  instructors.delete("Josh")
+def using_delete(instructors,"Steven")
+  instructors.delete
 end
 
 famous_robots = ["Johnny 5", "R2D2", "Robocop"]
