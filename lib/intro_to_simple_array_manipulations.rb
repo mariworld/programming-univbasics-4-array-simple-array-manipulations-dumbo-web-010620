94354c0ec868f05a6dd4f7fcab32a13594a23903
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-29 15:50:15 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,33 @@
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+next_color = "violet"
+
+def using_push(colors_in_the_rainbow,next_color)
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(colors_in_the_rainbow,next_color)
+  colors_in_the_rainbow.unshift(next_color)
+end
+
+def using_pop(colors_in_the_rainbow)
+  colors_in_the_rainbow.pop
+end
+
+def pop_with_args(colors_in_the_rainbow)
+  colors_in_the_rainbow.pop(2)
+end
+
+def using_shift(colors_in_the_rainbow)
+  colors_in_the_rainbow.shift
+end
+
+def shift_with_args(colors_in_the_rainbow)
+  colors_in_the_rainbow.shift(2)
+end
+
+def using_concat(colors_in_the_rainbow,)
+  
+  
+  
+  
+  
