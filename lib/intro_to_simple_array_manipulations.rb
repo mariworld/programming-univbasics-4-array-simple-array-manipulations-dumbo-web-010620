dcf4e41b9f487384c8334cce1a24a25636be4a4a
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-29 16:26:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -52,7 +52,8 @@ def using_delete(instructors,repeated_name)
 end
 
 famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-def using_delete_at(famous_robots)
-  famous_robots.delete_at(2)
+integer = 2
+def using_delete_at(famous_robots, integer)
+  famous_robots.delete_at(integer)
 end
   
