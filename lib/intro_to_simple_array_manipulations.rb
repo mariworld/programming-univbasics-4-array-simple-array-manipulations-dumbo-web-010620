520663380ac345c114b71faaf385693aaf5d2970
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-29 15:52:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,6 @@
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
 next_color = "violet"
+my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
 
 def using_push(colors_in_the_rainbow,next_color)
   colors_in_the_rainbow.push(next_color)
@@ -25,8 +26,9 @@ def shift_with_args(colors_in_the_rainbow)
   colors_in_the_rainbow.shift(2)
 end
 
-def using_concat(colors_in_the_rainbow,)
-  
+def using_concat(colors_in_the_rainbow,my_favorite_cities)
+  colors_in_the_rainbow.concat(my_favorite_cities)
+end
   
   
   
